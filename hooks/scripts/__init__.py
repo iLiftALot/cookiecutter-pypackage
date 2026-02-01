8f9b1326bc25fd10a04edb00c16dbcc987e529a6
Nick Corbin <nickcorbin17@yahoo.com>
2026-01-31 19:06:01 -0500

    Remove unused iTerm2 API imports from __init__.py

--- hooks/scripts/__init__.py
+++ hooks/scripts/__init__.py
@@ -13,20 +13,15 @@ from .github.shared_types import (
     TextField,
     place,
 )
-from .iterm.iterm2_api import GlobaliTermState, run_iterm_setup
-from .iterm.utils import poly_modal_alert_handler, send_command_to_iterm
+
 
 __all__ = [
     "create_github_repository",
-    "GlobaliTermState",
     "ButtonField",
     "TextField",
     "ComboBoxField",
     "CheckBoxField",
     "GitHubRepoConfig",
-    "run_iterm_setup",
-    "send_command_to_iterm",
-    "poly_modal_alert_handler",
     "show_form_dialog",
     "FormResult",
     "FormInputs",
